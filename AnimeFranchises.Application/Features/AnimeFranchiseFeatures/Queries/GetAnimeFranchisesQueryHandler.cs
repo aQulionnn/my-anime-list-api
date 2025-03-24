adf2869f4e13f0a9abbf78e6c1a4f26812182857
bekasker <2004bek.asker@mail.ru>
2025-03-24 21:37:22 +0500

    feat(anime-franchises): add caching to GetAnimeFranchises and GetAnimeFranchiseInfos queries

--- AnimeFranchises.Application/Features/AnimeFranchiseFeatures/Queries/GetAnimeFranchisesQueryHandler.cs
+++ AnimeFranchises.Application/Features/AnimeFranchiseFeatures/Queries/GetAnimeFranchisesQueryHandler.cs
@@ -1,20 +1,32 @@
 using AnimeFranchises.Application.Dtos.AnimeFranchiseDtos;
+using AnimeFranchises.Application.Services;
 using AnimeFranchises.Domain.Interfaces;
 using AutoMapper;
 using MediatR;
 
 namespace AnimeFranchises.Application.Features.AnimeFranchiseFeatures.Queries;
 
-public class GetAnimeFranchisesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) 
+public class GetAnimeFranchisesQueryHandler
+    (IUnitOfWork unitOfWork, IMapper mapper, ICacheService cache) 
     : IRequestHandler<GetAnimeFranchisesQuery, IEnumerable<AnimeFranchiseResponseDto>>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly IMapper _mapper = mapper;
+    private readonly ICacheService _cache = cache;
     
     public async Task<IEnumerable<AnimeFranchiseResponseDto>> Handle(GetAnimeFranchisesQuery request, CancellationToken cancellationToken)
     {
+        var cachedAnimeFranchises = await _cache.GetDataAsync<IEnumerable<AnimeFranchiseResponseDto>>("anime-franchises");
+        if (cachedAnimeFranchises is not null)
+        {
+            return cachedAnimeFranchises;    
+        }
+        
         var animeFranchises = await _unitOfWork.AnimeFranchiseRepository.GetAllAsync();
-        return _mapper.Map<IEnumerable<AnimeFranchiseResponseDto>>(animeFranchises);
+        var result = _mapper.Map<IEnumerable<AnimeFranchiseResponseDto>>(animeFranchises);
+        await _cache.SetDataAsync("anime-franchises", result);
+        
+        return result;
     }
 }
 
