3c151e624ead88f9553104829800def2eef7c4df
bekasker <2004bek.asker@mail.ru>
2025-03-26 23:24:10 +0500

    feat(anime-series): implement service registrations for application, infrastructure, and presentation layers

--- AnimeSeries.Application/DependencyInjection.cs
+++ AnimeSeries.Application/DependencyInjection.cs
@@ -1,6 +1,21 @@
-namespace AnimeExtras.Application;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace AnimeSeries.Application;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+        
+        services.AddAutoMapper(assembly);
+        services.AddValidatorsFromAssembly(assembly);
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(assembly);
+        });
+        
+        return services;
+    }
 }
--- AnimeSeries.Infrastructure/DependencyInjection.cs
+++ AnimeSeries.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,37 @@
-namespace AnimeExtras.Infrastructure;
+using AnimeSeries.Application.Services;
+using AnimeSeries.Domain.Interfaces;
+using AnimeSeries.Infrastructure.Data;
+using AnimeSeries.Infrastructure.Repositories;
+using AnimeSeries.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace AnimeSeries.Infrastructure;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AnimeSeriesDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Database"),
+                sqlOptions => sqlOptions.MigrationsHistoryTable("__EFMigrationsHistory_AnimeSeries"));
+        });
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IAnimeSerialRepository, AnimeSerialRepository>();
+        services.AddScoped<IAnimeSerialInfoRepository, AnimeSerialInfoRepository>();
+        services.AddScoped<IReWatchedAnimeSerialRepository, ReWatchedAnimeSerialRepository>();
+        
+        services.AddScoped<ICacheService, RedisCacheService>();
+        
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = configuration.GetConnectionString("Redis");
+            options.InstanceName = configuration["Redis:InstanceName"];
+        });
+        
+        return services;
+    }
 }
