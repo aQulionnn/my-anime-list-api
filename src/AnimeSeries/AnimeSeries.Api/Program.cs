51f7abd9682bdb34c147128ec1fbe89f07d554b0
bekasker <2004bek.asker@mail.ru>
2025-03-27 22:30:34 +0500

    chore(anime-series): configure startup project

--- src/AnimeSeries/AnimeSeries.Api/Program.cs
+++ src/AnimeSeries/AnimeSeries.Api/Program.cs
@@ -1,6 +1,31 @@
+using AnimeSeries.Application;
+using AnimeSeries.Infrastructure;
+using AnimeSeries.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddPresentation();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
