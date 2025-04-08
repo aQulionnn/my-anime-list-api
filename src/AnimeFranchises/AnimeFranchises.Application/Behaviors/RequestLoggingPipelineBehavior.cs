f3f7e7f1ac631392277f77bed007bf68da4499c9
bekasker <2004bek.asker@mail.ru>
2025-04-08 21:42:25 +0500

    feat(anime-franchises): enhance MediatR request logging behavior with success and error details

--- src/AnimeFranchises/AnimeFranchises.Application/Behaviors/RequestLoggingPipelineBehavior.cs
+++ src/AnimeFranchises/AnimeFranchises.Application/Behaviors/RequestLoggingPipelineBehavior.cs
@@ -1,21 +1,27 @@
+using AnimeFranchises.Domain.Shared;
 using MediatR;
 using Serilog;
 
 namespace AnimeFranchises.Application.Behaviors;
-
-public sealed class RequestLoggingPipelineBehavior<TRequest, TResponse> 
-    : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : class
-    where TResponse : class
-{
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-    {
-        string requestName = typeof(TRequest).Name;
-        
-        Log.Information("Processing request {requestName} {@request}", requestName, request);
-        
-        TResponse response = await next();
-
-        return response;
-    }
-}
+ 
+ public sealed class RequestLoggingPipelineBehavior<TRequest, TResponse> 
+     : IPipelineBehavior<TRequest, TResponse>
+     where TRequest : class
+     where TResponse : Result<TResponse>
+ {
+     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+     {
+         string requestName = typeof(TRequest).Name;
+         
+         Log.Information("Processing request {requestName} {@request}", requestName, request);
+         
+         TResponse response = await next();
+ 
+         if (!response.IsSuccess)
+             Log.Error("Request {requestName} failed with error {@error}", requestName, response.Error);
+         else
+             Log.Information("Request {requestName} succeeded with result {@data}", requestName, response.Data);
+         
+         return response;
+     }
+ }
