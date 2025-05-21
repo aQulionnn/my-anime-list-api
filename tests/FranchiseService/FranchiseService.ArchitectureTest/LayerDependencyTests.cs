86418d8c3b40b46221e71a70732839246ab4778a
bekasker <2004bek.asker@mail.ru>
2025-05-21 11:25:31 +0500

    feat(franchise-architecture-test): add test enforcing MessageBroker dependency rules

--- tests/FranchiseService/FranchiseService.ArchitectureTest/LayerDependencyTests.cs
+++ tests/FranchiseService/FranchiseService.ArchitectureTest/LayerDependencyTests.cs
@@ -11,6 +11,29 @@ public class LayerDependencyTests
     private const string ApiNamespace = "FranchiseService.Api";
 
     [Fact]
+    public void MessageBroker_Should_Not_HaveDependencyOnLayers()
+    {
+        var assembly = typeof(MessageBroker.AssemblyReference).Assembly;
+
+        var layers = new[]
+        {
+            DomainNamespace,
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            ApiNamespace
+        };
+        
+        var result = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(layers)
+            .GetResult();
+        
+        Assert.True(result.IsSuccessful);  
+    }
+    
+    [Fact]
     public void SharedKernel_Should_Not_HaveDependencyOnLayers()
     {
         var assembly = typeof(SharedKernel.AssemblyReference).Assembly;
